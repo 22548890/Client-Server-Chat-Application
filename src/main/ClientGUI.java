56832a55c2b43afd3310f86f23dffd8049ac99b8
Sass, MF, Mr [22548890@sun.ac.za] <22548890@sun.ac.za>
2022-08-02 19:09:45 +0200

    list users to be implement and unique username

--- src/main/ClientGUI.java
+++ src/main/ClientGUI.java
@@ -2,8 +2,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.Scanner;
-
+import java.util.ArrayList;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -13,13 +12,15 @@ public class ClientGUI implements ActionListener {
     private ObjectInputStream objectInputStream;
     private ObjectOutputStream objectOutputStream;
     private String username;
-    private String strAllMessages="";
+    //private String strAllMessages="";
     private String msg="";
     private JFrame frame;
 
     private JTextArea  enteredText;
     private JTextField typedText;
-    static ClientGUI client;
+    private DefaultListModel listModel;
+    private JList usersList;
+    static ClientGUI client; 
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -37,20 +38,32 @@ public class ClientGUI implements ActionListener {
     
     public ClientGUI(Socket socket, String username) {
         frame = new JFrame();
+        //frame.setSize(500, 400);
 
         JButton btn = new JButton("send");
         btn.addActionListener(this);
         
         enteredText = new JTextArea(10, 32);
         typedText   = new JTextField(32);
+        //adding curent username
+        //TODO add all 
+        listModel = new DefaultListModel();
+        listModel.addElement("Online Users:");
+        listModel.addElement(username);
+        //listModel.remove("")
 
+        usersList = new JList(listModel);
+        
         enteredText.setEditable(false);
+        usersList.setFocusable(false);
         enteredText.setBackground(Color.LIGHT_GRAY);
         typedText.addActionListener(this);
+
 
         Container content = frame.getContentPane();
         content.add(new JScrollPane(enteredText), BorderLayout.CENTER);
         content.add(typedText, BorderLayout.SOUTH);
+        content.add(usersList, BorderLayout.EAST);
         typedText.requestFocusInWindow();
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -85,6 +98,10 @@ public class ClientGUI implements ActionListener {
                     String clientTo = str[0].substring(1);
                     textToSend = str[1];
                     messageToSend = new Message(textToSend, username, clientTo);
+                }
+                else if(textToSend.equals("\\addUser")) {//to remove just testing
+                   listModel.addElement("New User added");   
+                   
                 } else {
                     messageToSend = new Message(textToSend, username);
                 }
